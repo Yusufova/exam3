f81e7695c3d87592840005efabcedbce003dc974
Yusufova <yusufova.odina11@gmail.com>
2023-12-23 12:06:57 +0500

    exam

---
+++ main.py
@@ -0,0 +1,37 @@
+#1
+# list = [1,2,3,4,6,7,8,9,10]
+#
+# def missed_num():
+#     for x in range(1,11):
+#         if x not in list:
+#             return x
+# missed_num()
+#
+
+#2
+
+# m = input("Enter any nums:")
+# d = m // 5
+# print(d)
+
+
+
+#3
+# u = int(input("Enter percent:"))
+# r = [56,78,90,6,4]
+#
+# def percent():
+#     s = []
+#     for x in r:
+#         s.append(u / 100 + x)
+#         return r
+#
+
+#4
+find_odd = [1,1,2,3,3,4,4,5,5,6,6]
+
+
+for x in find_odd:
+    find_odd.count(find_odd)
+    if x in find_odd:
+        print(find_odd)
